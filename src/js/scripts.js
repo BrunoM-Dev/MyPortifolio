fbf3b9087ce84f5c41d12c2af98b980162c48b9f
Bruno Macedo <devbrunomacedo@gmail.com>
2024-01-17 11:48:28 -0300

    correções de bugs

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,19 @@
-const menu = {
-    tag: 'header',
-    classeMobile: '.mobile-menu',
-}
+const menu = {classeMobile: '.mobile-menu'}
 
 function menuShow(){    
     const menuMobile = document.querySelector(menu.classeMobile);
-    console.log('ola')
 
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector('.icone-menu-mobile').src = "src/images/menu-mobile/button_mobile.svg";
     } else {
+        document.querySelector('header').classList.remove('esconder')
         menuMobile.classList.add('open');
         document.querySelector('.icone-menu-mobile').src = "src/images/menu-mobile/close_mobile.svg";
     }
+
+    setTimeout( () => {
+        document.querySelector('header').classList.add('esconder')
+        document.querySelector('.icone-menu-mobile').src = "src/images/menu-mobile/button_mobile.svg";
+    }, 4000)
 }
